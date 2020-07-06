d2c24c468f090392d1ed20de4625194593064c81
Ess91 <eagyare91@gmail.com>
2020-07-06 23:38:28 +0100

    Weather API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
-if (hours < 10){
-    hours = `0${hours}`
-}
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
 
   let minutes = date.getMinutes();
   if (minutes < 10) {
-      minutes = `0${minutes}`
+    minutes = `0${minutes}`;
   }
 
   let days = [
@@ -17,7 +17,7 @@ if (hours < 10){
     "Wednesday",
     "Thursday",
     "Friday",
-    "Saturday"
+    "Saturday",
   ];
   let day = days[date.getDay()];
   return `${day}, ${hours}:${minutes}`;
@@ -32,19 +32,22 @@ function displayTemperature(response) {
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
- 
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`); 
-iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "c788fbd12920cbf73a67468fe8b0facb";
-let city = "Accra"
+let city = "Accra";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 //console.log(apiUrl);
