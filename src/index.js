c31e215eea8a4d1e52a4fc7549d6b3b7ed6426d8
Ess91 <eagyare91@gmail.com>
2020-07-08 20:33:45 +0100

    taken a few steps back

--- src/index.js
+++ src/index.js
@@ -48,19 +48,19 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
-function displayForecast(response) {
-  console.log(response.data);
-  let forecastElement = document.querySelector("#forecast");
-  forecastElement.innerHTML = 
-}
+//function displayForecast(response) {
+  //console.log(response.data);
+  //let forecastElement = document.querySelector("#forecast");
+  //forecastElement.innerHTML = 
+//}
 
 function search(city) {
   let apiKey = "c788fbd12920cbf73a67468fe8b0facb";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 
-  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
+ // apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  //axios.get(apiUrl).then(displayForecast);
 }
 
 function handleSubmit(event) {
